a72828c1f58c18f3035b4dcb5348ad69fddd8d01
pauloAlmeida01 <paulo.rsantos@sptech.school>
2023-11-20 11:44:35 -0300

    Repository de funcionario

---
+++ app/src/main/java/br/com/sanittas/app/repository/FuncionarioRepository.java
@@ -0,0 +1,10 @@
+package br.com.sanittas.app.repository;
+
+import br.com.sanittas.app.model.Funcionario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FuncionarioRepository extends JpaRepository<Funcionario, Integer> {
+
+}
